083dff9e447b84860d617e0845b4e1c17922590c
mattsunkun <mattsunkun1221@gmail.com>
2024-05-04 15:26:30 +0900

    <mod> zindex

--- adkiller/src/pages/Home.tsx
+++ adkiller/src/pages/Home.tsx
@@ -3,23 +3,10 @@ import { Button } from "@mui/material";
 const Home = () => {
   return (
     <>
-      <h1>
-        タイトル
-      </h1>
       <Button
         component={Link}
         to={"/stage"}>
         stageへ
-      </Button>
-      <Button
-        component={Link}
-        to={"/chahan"}>
-        チャーハン
-      </Button>
-      <Button
-        component={Link}
-        to={"/hiranan"}>
-        ひらなん
       </Button>
     </>
   );
--- adkiller/src/pages/Stage.tsx
+++ adkiller/src/pages/Stage.tsx
@@ -1,28 +1,12 @@
-import { Link } from "react-router-dom";
-import { Button } from "@mui/material";
 import Generator from "../components/Ads/Generator";
-import Ad from "../components/Ads/Ad";
+
 const Stage = () => {
   return (
     <>
-      <h1>
-        ステージ
-      </h1>
-
-      <Button
-        component={Link}
-        to={"/"}>
-        homeへ
-      </Button>
+      {/* ゲーム班 */}
 
       <Generator />
-      {/* <Ad
-        strImg={`${process.env.PUBLIC_URL}/neko.png`}
-        strLink="https://flobal.jp/160.html"
-        numCloseX={100}
-        numCloseY={100}
-        strClass="move" */}
-      {/* /> */}
+
     </>
   );
 };
